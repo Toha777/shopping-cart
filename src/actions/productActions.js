8776448a041daeb40b42e1e39fd1dde8a432a049
Anthon Tchernukha <toha77747@gmail.com>
2021-04-19 15:52:21 +0300

    filter to redux

--- src/actions/productActions.js
+++ src/actions/productActions.js
@@ -1,11 +1,35 @@
-// import { FETCH_PRODUCTS } from "../types";
-import { fetchProductsAction } from "../reducers/productReducers";
+// 
+import { fetchProductsAction,filterProductsBySizeAction,orderProductsByPriceAction } from "../reducers/productReducers";
 
 export const fetchProducts = () => dispatch => {
     fetch("http://localhost:5000/api/products")
     .then(res => res.json())
     .then(json => dispatch(fetchProductsAction(json)))
-    // const res = await fetch("http://localhost:5000/api/products");
-    // const data = await res.json();
-    // dispatch(fetchProductsAction(data));
+}
+
+export const filterProducts = (products, size) => dispatch => {
+    dispatch(filterProductsBySizeAction({
+        size: size,
+        items: size === ""
+                ? products
+                : products.filter( x => x.availableSizes.indexOf(size) >= 0 )
+
+    }))
+}
+
+export const sortProducts = (filteredProducts, sort) => dispatch => {
+    const sortedProducts = filteredProducts.slice();
+    if(sort === "latest") {
+        sortedProducts.sort((a,b) => a.id  > b.id ? 1 : -1)
+    } else {
+        sortedProducts.sort((a,b) => (
+            sort === "lowest"
+            ? a.price > b.price ? 1 : -1
+            : a.price > b.price ? -1 : 1
+        ))
+    }
+    dispatch(orderProductsByPriceAction({
+        sort: sort,
+        items: sortedProducts
+    }))
 }
--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,20 +1,31 @@
 import React from 'react'
+import { useDispatch, useSelector } from 'react-redux';
+import { sortProducts, filterProducts } from '../actions/productActions';
 
 export default function Filter(props) {
+     const dispatch = useDispatch();
+     var size = useSelector( state => state.products.size );
+     var sort = useSelector( state => state.products.sort );
+     const items = useSelector( state => state.products.items );
+     const filteredItems  = useSelector( state => state.products.filteredItems );
+     const count = filteredItems.length;
+     const sortItems = sort => dispatch(sortProducts(filteredItems,sort));
+     const filterItems = size => dispatch(filterProducts(items,size));
+     
     return (
         <div className="filter">
-            <div className="filter-result">{props.count} Products</div>
+            <div className="filter-result">{count} Products</div>
             <div className="filter-sort">
                 Order{" "}
-                <select value={props.size} onChange={props.sortProducts}>
-                    <option>Latest</option>
+                 <select value={sort} onChange={(e)=>sortItems(e.target.value)}>
+                    <option value="latest">Latest</option>
                     <option value="lowest">Lowest</option>
                     <option value="highest">Highest</option>
                 </select>
             </div>
             <div className="filter-size">
                 Filter{" "}
-                <select value={props.size} onChange={props.filterProducts}>
+                <select value={size} onChange={(e)=>filterItems(e.target.value)}>
                     <option value="">ALL</option>
                     <option value="XS">XS</option>
                     <option value="S">S</option>
